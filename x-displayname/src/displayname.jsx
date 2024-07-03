ccba3163dbf8e2379dcb5621236f11f8fcb97232
NaginiKamsala <naginisanju@gmail.com>
2024-07-03 18:13:08 +0530

    change 3

--- x-displayname/src/displayname.jsx
+++ x-displayname/src/displayname.jsx
@@ -24,7 +24,7 @@ const DisplayName = () => {
 
   return (
     <div>
-      <h1>FullName Display</h1>
+      <h1>Full Name Display</h1>
       <form onSubmit={handleSubmit}>
         <div>
           <label>
