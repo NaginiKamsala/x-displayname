322a3f5e4926e2ac67600c7e21508f401f245eff
NaginiKamsala <naginisanju@gmail.com>
2024-07-03 17:52:43 +0530

    x-displayname minibuildout

---
+++ x-displayname/src/App.jsx
@@ -0,0 +1,12 @@
+import DisplayName from "./displayname";
+function App() {
+  return (
+    <>
+      <div>
+        <DisplayName />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ x-displayname/src/displayname.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const DisplayName = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFullName(`${firstName} ${lastName}`);
+  };
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>FullName Display</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            First Name:
+            <input
+              type="text"
+              value={firstName}
+              onChange={handleFirstNameChange}
+              required
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Last Name:
+            <input
+              type="text"
+              value={lastName}
+              onChange={handleLastNameChange}
+              required
+            />
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div>{fullName && <h2>Full Name: {fullName}</h2>}</div>
+    </div>
+  );
+};
+
+export default DisplayName;
